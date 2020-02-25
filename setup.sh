6fc0b3f2a141dd7a1bf7165e961d9f57c56f53c1
Heng Ma <maheng1991@hotmail.com>
2020-02-25 16:21:35 -0600

    addded git name and email

--- setup.sh
+++ setup.sh
@@ -1,11 +1,22 @@
 #!/bin/bash 
 
-echo "Setting up vim" 
+echo "Setting up vim..." 
 cat ./vimrc > $HOME/.vimrc 
 
-echo "Setting up tmux" 
+
+echo "Setting up git..."
+git config --global user.name "Heng Ma"
+git config --global user.email maheng1991@hotmail.com 
+
+
+echo "Setting up tmux..." 
 conda install -y -c conda-forge tmux 
 mkdir -p Pkgs && cd Pkgs 
 git clone https://github.com/hengma1001/tmux-config.git 
 cd tmux-config 
 ./install.sh 
+cd 
+
+
+
+
